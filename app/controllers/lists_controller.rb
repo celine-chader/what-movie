7e176e1569debfb0f1d0edf43d531d62e6e01377
celine chader <chader.celine@gmail.com>
2023-08-31 17:58:47 +0200

    model and controller lists with methods done

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  def index
+    @mlists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/movies", to: "movies#index"
   get "/movies/:id", to: "movies#show"
+  get "/lists", to: "lists#index"
+  get "/lists", to: "lists#new", as: :new_list
+  get "/lists/:id", to: "lists#show"
+  post "/lists", to: "lists#create"
+  get "/lists/:id/edit", to: "lists#edit", as: :edit_list
+  patch "/lists/:id", to: "lists#update"
+  delete "/lists/:id", to: "lists#destroy"
+
 end

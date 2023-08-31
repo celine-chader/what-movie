1a729f10b6876a6d97bd8ff4cc84626e1324b173
celine chader <chader.celine@gmail.com>
2023-08-31 17:37:13 +0200

    movies controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "pages#home"
+  get "/movies", to: "movies#index"
+  get "/movies/:id", to: "movies#show"
 end

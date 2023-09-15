583dee58cc7179b2cb0fca680f746dd0cbdb1635
celine chader <chader.celine@gmail.com>
2023-09-15 10:57:25 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "json"
+require "open-uri"
+
+puts "deleting data"
 Movie.destroy_all
-Movie.create(title: "Wonder Woman 1984", description: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s")
-Movie.create(title: "The Shawshank Redemption", description: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison")
-Movie.create(title: "Titanic", description: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.")
-Movie.create(title: "Ocean's Eight", description: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.")
+puts "#{Movie.all.count}"
+
+url = "https://tmdb.lewagon.com/movie/popular"
+movies = JSON.parse(URI.open(url).read)
+
+movies["results"].each do |movie|
+  puts "creating movie"
+  Movie.create(
+    title: movie["original_title"],
+    year: movie["release_date"],
+    description: movie["overview"],
+  )
+end
+
+puts "#{Movie.all.count}"
 puts "finished seeding"
